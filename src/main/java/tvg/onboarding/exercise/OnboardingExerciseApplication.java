d9ebecccb6b0615a12790709fe5d9bfc5e1fd8f1
Filipa <filipatitan@gmail.com>
2022-07-06 14:10:38 +0100

    first commit

---
+++ src/main/java/tvg/onboarding/exercise/OnboardingExerciseApplication.java
@@ -0,0 +1,13 @@
+package tvg.onboarding.exercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnboardingExerciseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnboardingExerciseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tvg/onboarding/exercise/OnboardingExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package tvg.onboarding.exercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnboardingExerciseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
