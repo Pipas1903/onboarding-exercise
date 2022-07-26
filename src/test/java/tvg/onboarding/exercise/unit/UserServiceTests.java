e3aec54f2f05e6dffcaccfd3f4398a75627e56c3
Filipa <filipatitan@gmail.com>
2022-07-26 15:46:37 +0100

    fixed user service test configuration

--- src/test/java/tvg/onboarding/exercise/unit/UserServiceTests.java
+++ src/test/java/tvg/onboarding/exercise/unit/UserServiceTests.java
@@ -1,10 +1,8 @@
 package tvg.onboarding.exercise.unit;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import tvg.onboarding.exercise.converter.UserConverter;
