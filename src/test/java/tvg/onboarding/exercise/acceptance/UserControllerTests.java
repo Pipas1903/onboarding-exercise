cb187b925afe52e015c015fca42c35024af55d6e
Filipa <filipatitan@gmail.com>
2022-07-26 15:49:27 +0100

    added rest assured dependency

--- src/test/java/tvg/onboarding/exercise/acceptance/UserControllerTests.java
+++ src/test/java/tvg/onboarding/exercise/acceptance/UserControllerTests.java
@@ -1,4 +1,21 @@
 package tvg.onboarding.exercise.acceptance;
 
+import org.aspectj.lang.annotation.Before;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class UserControllerTests {
+
+    @LocalServerPort
+    private int port;
+
+    @Before
+    public void setUp() {
+        RestAssured.port = port;
+    }
+
 }
